ef06253a90c2ba99850bcfb8a6042bc00ec80230
sabinbabu <sabinacharya444@gmail.com>
2025-02-24 21:39:09 +1100

    portfolio card clickable added

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -1,37 +1,43 @@
 /* eslint-disable react/prop-types */
-import { Card, Badge, Stack, Button } from "react-bootstrap";
+import { Card, Badge, Stack, Button, NavLink } from "react-bootstrap";
 import { Globe2, Github } from "react-bootstrap-icons";
 
 const PortfolioCard = ({ img, title, liveLink, gitLink, techStack }) => {
   return (
-    <Card
-      style={{ width: "22rem", backgroundColor: "#4a4a4a", color: "white" }}
-      className="border border-0"
+    <NavLink
+      href={liveLink}
+      target="_blank"
+      style={{ textDecoration: "none", color: "inherit" }}
     >
-      <Card.Img variant="top" src={img} />
-      <Card.Body>
-        <Card.Text>
+      <Card
+        style={{ width: "22rem", backgroundColor: "#4a4a4a", color: "white" }}
+        className="border border-0 "
+      >
+        <Card.Img variant="top" src={img} />
+        <Card.Body>
+          <Card.Text>
+            <Stack direction="horizontal" gap={2}>
+              {techStack.map((item) => (
+                <Badge key={item.id} bg="primary">
+                  {item.name}
+                </Badge>
+              ))}
+            </Stack>
+          </Card.Text>
+          <Card.Title>{title}</Card.Title>
+
           <Stack direction="horizontal" gap={2}>
-            {techStack.map((item) => (
-              <Badge key={item.id} bg="primary">
-                {item.name}
-              </Badge>
-            ))}
+            <Button variant="light" target="_blank" href={liveLink}>
+              <Globe2 />
+            </Button>
+
+            <Button variant="light" target="_blank" href={gitLink}>
+              <Github />
+            </Button>
           </Stack>
-        </Card.Text>
-        <Card.Title>{title}</Card.Title>
-
-        <Stack direction="horizontal" gap={2}>
-          <Button variant="light" target="_blank" href={liveLink}>
-            <Globe2 />
-          </Button>
-
-          <Button variant="light" target="_blank" href={gitLink}>
-            <Github />
-          </Button>
-        </Stack>
-      </Card.Body>
-    </Card>
+        </Card.Body>
+      </Card>
+    </NavLink>
   );
 };
 
