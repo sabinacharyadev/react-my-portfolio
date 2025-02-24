ae036b6785fdeb097dc7dc521d3c312050b7e2de
sabinbabu <sabinacharya444@gmail.com>
2025-02-24 19:48:22 +1100

    social profiles added

--- src/components/ProfileComponent.jsx
+++ src/components/ProfileComponent.jsx
@@ -1,5 +1,6 @@
-import { Image } from "react-bootstrap";
+import { Button, Image, Stack } from "react-bootstrap";
 import ProfileImage from "../assets/profile-image.png";
+import { Linkedin, Github, StackOverflow } from "react-bootstrap-icons";
 const ProfileComponent = () => {
   return (
     <>
@@ -13,6 +14,29 @@ const ProfileComponent = () => {
         <div>
           <h1>Sabin Acharya</h1>
           <p>sabinacharyadev</p>
+
+          <Stack className="d-flex flex-row justify-content-start justify-content-lg-center gap-3">
+            <Button
+              target="_blank"
+              href="https://www.linkedin.com/in/sabinacharya/"
+            >
+              <Linkedin />
+            </Button>
+            <Button
+              target="_blank"
+              variant="dark"
+              href="https://github.com/sabinacharyadev"
+            >
+              <Github />
+            </Button>
+            <Button
+              target="_blank"
+              variant="warning"
+              href="https://stackoverflow.com/users/10009481/sabin-acharya"
+            >
+              <StackOverflow />
+            </Button>
+          </Stack>
         </div>
       </div>
     </>
