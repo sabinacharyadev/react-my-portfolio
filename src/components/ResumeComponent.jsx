8e2f048920e67cb58650e2529792e48b60ed627d
sabinbabu <sabinacharya444@gmail.com>
2025-02-25 19:05:13 +1100

    download button added

--- src/components/ResumeComponent.jsx
+++ src/components/ResumeComponent.jsx
@@ -1,8 +1,14 @@
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
+import { FilePdf } from "react-bootstrap-icons";
+import Resume from "../assets/myresume.pdf";
 const ResumeComponent = () => {
   return (
     <Container className="mt-2">
       <h1 className="pt-3">Resume</h1>
+      <Button className="p-3 float-end" href={Resume} download>
+        <FilePdf size={30} />
+        &nbsp;&nbsp;Download CV
+      </Button>
     </Container>
   );
 };
